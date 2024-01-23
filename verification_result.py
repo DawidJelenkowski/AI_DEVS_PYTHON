358945bc1f1a2a3a853e0f94c1d70535d075b6da
Dawid Jeleńkowski <jelenkowskidawid@pm.me>
2024-01-23 20:29:10 +0100

    push

--- verification_result.py
+++
@@ -1,7 +0,0 @@
-class VerificationResult:
-    def __init__(self, json):
-        self.id = json['id']
-        self.model = json['model']
-        self.results = []
-        for result in json['results']:
-            self.results.append(result['flagged'])
